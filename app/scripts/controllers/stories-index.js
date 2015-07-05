02e8cec12c29e61ccb79ef230390e7a49a2cb642
Jared E Morgan <jmorgan@mchost.net>
2015-07-04 20:40:15 -0500

    Update StoriesIndexCtrl to use Story resource

--- app/scripts/controllers/stories-index.js
+++ app/scripts/controllers/stories-index.js
@@ -8,9 +8,6 @@
  * Controller of the StoriesBy2 app
  */
 angular.module('StoriesBy2')
-  .controller('StoriesIndexCtrl', function ($scope) {
-    $scope.stories = [
-      'First',
-      'Second'
-    ];
+  .controller('StoriesIndexCtrl', function ($scope, Story) {
+    $scope.stories = Story.query();
   });
--- test/spec/controllers/stories-index.js
+++ test/spec/controllers/stories-index.js
@@ -1,22 +1,45 @@
 'use strict';
 
 describe('Controller: StoriesIndexCtrl', function () {
+  var $httpBackend, $rootScope, createController, config;
 
   // load the controller's module
   beforeEach(module('StoriesBy2'));
-
-  var StoriesIndexCtrl,
-    scope;
+  beforeEach(module('app.config'));
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    StoriesIndexCtrl = $controller('StoriesIndexCtrl', {
-      $scope: scope
-    });
+  beforeEach(inject(function($injector) {
+    $httpBackend = $injector.get('$httpBackend');
+
+    $rootScope = $injector.get('$rootScope');
+
+    var $controller = $injector.get('$controller');
+
+    createController = function() {
+      return $controller('StoriesIndexCtrl', { '$scope' : $rootScope });
+    };
+
+    config = $injector.get('app.config');
   }));
 
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
   it('should attach a list of stories to the scope', function () {
-    expect(scope.stories.length).toBe(2);
+    $httpBackend.expect('GET', config.apiUrl + '1/stories')
+      .respond(
+        200, '{"response":[' +
+          '{"id":1,"parent_id":null,"body":"a b","level":1,"words":2},' +
+          '{"id":2,"parent_id":1,"body":"a c b d","level":2,"words":4}' +
+        '],"count":2}'
+      );
+
+    var controller = createController();
+    $httpBackend.flush();
+    expect($rootScope.stories.length).toBe(2);
+    expect($rootScope.stories[0].body).toBe('a b');
+    expect($rootScope.stories[1].body).toBe('a c b d');
   });
 });
