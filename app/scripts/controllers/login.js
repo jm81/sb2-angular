418b6c5365bb34cfb081e749e68fe966653a740b
Jared E Morgan <jmorgan@mchost.net>
2016-01-31 20:15:17 -0600

    LoginCtrl.authenticate - redirect to profile/new if no profile

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,13 +8,17 @@
  * Authentication, using satellizer
  */
 angular.module('StoriesBy2')
-  .controller('LoginCtrl', function ($scope, $auth, toastr, Session) {
-    $scope.authenticate = function(provider) {
-      $auth.authenticate(provider).then(function() {
-        toastr.success('You have successfully signed in with ' + provider);
-        Session.get().then(function(data) {
-          $scope.setCurrentSession(data);
+  .controller('LoginCtrl',
+    function ($scope, $auth, $location, toastr, Session) {
+      $scope.authenticate = function(provider) {
+        $auth.authenticate(provider).then(function() {
+          toastr.success('You have successfully signed in with ' + provider);
+          Session.get().then(function(data) {
+            $scope.setCurrentSession(data);
+            if (!($scope.currentSession.profile_id))
+              $location.path('/profiles/new');
+          });
         });
-      });
-    };
-  });
+      };
+    }
+  );
